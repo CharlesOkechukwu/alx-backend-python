a388e33ddd972097d000996e81a8a256f13cc8ed
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-02 18:36:13 +0000

    create concat annotated function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""module for annotated concat function that concats two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """accepts two arguments str1 and returns concatenated string
+    made up of str1 and str2"""
+    return str1 + str2
