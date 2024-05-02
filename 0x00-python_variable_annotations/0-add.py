73ec38dca51171aaf012a4242fda2398a735de82
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-02 18:27:38 +0000

    create python add annotated function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/binenv python3
+"""module for add annotated function"""
+
+
+def add(a: float, b: float) -> float:
+    """add variables a and b of type float and return float"""
+    return a + b
