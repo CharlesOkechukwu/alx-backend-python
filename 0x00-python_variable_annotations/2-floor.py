61291d97a8f85729bd9ac755c9d7c2aa5772468b
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-02 18:45:46 +0000

    create floor annotated function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""python module for fllor annotated functions that return
+the floor of the float"""
+import math
+
+
+def floor(n: float) -> int:
+    """floor function which accepts a float n and returns the
+    floor ofn as an int"""
+    return math.floor(n)
