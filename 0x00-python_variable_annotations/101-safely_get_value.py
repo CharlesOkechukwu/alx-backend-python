cf27722806b16954c12cb4a6e01e01d358b5022a
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-03 01:40:56 +0000

    fix output error

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """module to return a value in a dictionary if key exists"""
-from typing import Mapping, Any, Typevar, Union
+from typing import Mapping, Any, TypeVar, Union
+
+
+T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] =
-         None) -> Union[Any, T]:
+                     None) -> Union[Any, T]:
     """accepts a dict, a key and a default argument and returns
     the value or None"""
     if key in dct:
