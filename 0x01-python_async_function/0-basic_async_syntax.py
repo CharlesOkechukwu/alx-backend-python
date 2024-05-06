9deed4441dada7b21becd5c3c61f5528cd062021
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-06 18:03:43 +0000

    create async coroutine that waits a randomly generated max_delay time and rtreturns wait time in seconds

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int) -> int:
+async def wait_random(max_delay: int = 10) -> float:
     """wait for a random amount of time not greater than max_delay"""
     wait: float = random.uniform(0, max_delay)
     await asyncio.sleep(wait)
