901c88b128a2da9dfc4fd3879abebdc1e2ee3a4d
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-28 05:04:25 +0100

    test get_json if it returns correct payload

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ import unittest
 from parameterized import parameterized
 from typing import (Dict, Tuple, Union)
 from unittest.mock import patch
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,3 +30,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """test if execption is correctly raised for nested_map function"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """class that contains methods to test get_json method"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url: str, test_payload: Dict[str, bool]
+                      ) -> None:
+        """test get_json that it returns the correct payload"""
+        conf = {'return_value.json.return_value': test_payload}
+        with patch('requests.get', autospec=True, **conf) as mock_req:
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_req.assert_called_once_with(test_url)
